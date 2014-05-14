24181fb966d9c606cd06f519336cf3fc8ffdd5b5
Barthélémy David <dbarthelemy@imakeit4u.com>
2014-05-14 23:31:23 +0200

    Express App
    Store contact email in Parse

---
+++ dbarthelemy/cloud/app.js
@@ -0,0 +1,62 @@
+
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+app.get('/hello', function(req, res) {
+  var highlight = 'Thanks!';
+  var message = 'I\'ll come back to you as soon as possible';
+
+  res.render('hello', { highlight: highlight, message: message });
+});
+
+app.post('/hello', function(req, res) {
+  var highlight = 'Ooops!';
+  var message = 'You did not enter a valid email... are you a bot?';
+  var email = req.body.email;
+
+  if (email != '') { // FIXME: Check the email
+    var Contact = Parse.Object.extend("Contact");
+    var aContact = new Contact();
+ 
+	aContact.set("email", email);
+ 
+	aContact.save(null, {
+  	  success: function(aContact) {
+        // Execute any logic that should take place after the object is saved.
+  	  },
+  	  error: function(aContact, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and description.
+        alert('Failed to create new object, with error code: ' + error.description);
+  	  }
+    });
+    
+    highlight = 'Great!';
+    message = 'I\'ll come back to you as soon as possible';
+  }
+
+  res.render('hello', { highlight: highlight, message: message });
+});
+
+// // Example reading from the request query string of an HTTP get request.
+// app.get('/test', function(req, res) {
+//   // GET http://example.parseapp.com/test?message=hello
+//   res.send(req.query.message);
+// });
+
+// // Example reading from the request body of an HTTP post request.
+// app.post('/test', function(req, res) {
+//   // POST http://example.parseapp.com/test (with request body "message=hello")
+//   res.send(req.body.message);
+// });
+
+// Attach the Express app to Cloud Code.
+app.listen();
